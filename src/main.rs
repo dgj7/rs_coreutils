f427274bbe65d30fd889ad8a08ebed9b91912d12
dg <garciad48879@gmail.com>
2024-01-01 20:15:16 -0500

    update readme; change printing of help/version

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,28 @@ fn count(config: &Config) -> Vec<Counts> {
     let mut result: Vec<Counts> = vec![];
 
     if config.show_version_exit {
-        println!("wc {}", VERSION);
+        println!("rwc {}", VERSION);
     } else if config.show_help_exit {
-        println!("help: todo");
+        println!("Usage: wc [OPTION]... [FILE]...");
+        println!("    or:  wc [OPTION]... --files0-from=F");
+        println!("Print newline, word, and byte counts for each FILE, and a total line if");
+        println!("more than one FILE is specified.  A word is a non-zero-length sequence of");
+        println!("characters delimited by white space.");
+        println!();
+        println!("    With no FILE, or when FILE is -, read standard input.");
+        println!();
+        println!("    The options below may be used to select which counts are printed, always in");
+        println!("the following order: newline, word, character, byte, maximum line length.");
+        println!("    -c, --bytes            print the byte counts");
+        println!("    -m, --chars            print the character counts");
+        println!("    -l, --lines            print the newline counts");
+        println!("    --files0-from=F    read input from the files specified by");
+        println!("NUL-terminated names in file F;");
+        println!("If F is - then read names from standard input");
+        println!("    -L, --max-line-length  print the maximum display width");
+        println!("    -w, --words            print the word counts");
+        println!("    --help     display this help and exit");
+        println!("    --version  output version information and exit");
     } else if config.show_bytes || config.show_chars || config.show_lines || config.show_words || config.show_max_line {
         // todo: stop using fake results here
         for (index, file_name) in config.file_paths.iter().enumerate() {
@@ -26,7 +45,7 @@ fn count(config: &Config) -> Vec<Counts> {
             result.push(counts);
         }
     } else {
-        // todo: is this the correct error message?  is there an error message at all?
+        // actual wc program waits for input; ours will just throw an error and exit
         panic!("no valid command line arguments provided");
     }
 
