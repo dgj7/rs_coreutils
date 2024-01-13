79b994f6c2c306265e9d3d059a113d140f6614cf
dg <garciad48879@gmail.com>
2024-01-12 22:18:53 -0500

    add test for month-name calc

--- src/months.rs
+++ src/months.rs
@@ -11,3 +11,31 @@ pub fn month_name(config: &MonthConfig, include_year: bool) -> String {
         month.to_string()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::config::MonthConfig;
+    use crate::months::month_name;
+
+    #[test]
+    #[should_panic]
+    fn test_month_0() {
+        month_name(&MonthConfig{month: 0, year: 2024}, true);
+    }
+
+    #[test]
+    fn test_month_1() {
+        assert_eq!("January 2024", month_name(&MonthConfig{month: 1, year: 2024}, true));
+    }
+
+    #[test]
+    fn test_month_12() {
+        assert_eq!("December", month_name(&MonthConfig{month: 12, year: 2024}, false));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_month_13() {
+        assert_eq!("", month_name(&MonthConfig{month: 13, year: 2024}, false));
+    }
+}
