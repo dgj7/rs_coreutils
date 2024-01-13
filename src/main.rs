97e3b2f78240dfbd5d2b517f66b196ab4ace238a
dg <garciad48879@gmail.com>
2024-01-12 19:49:02 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-extern crate string_builder;
-
-use crate::config::AppConfig;
-
 mod config;
 mod calc;
 mod formatter;
 
 fn main() {
     let args = std::env::args();
-    let config = AppConfig::new(args);
+    let config = config::AppConfig::new(args);
     let lines = formatter::format_months(config);
 
     lines.iter().for_each(|line| println!("{}", line));
