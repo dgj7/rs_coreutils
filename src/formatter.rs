0669d85badd42859f577e62a4236d7d47fd010e5
dg <garciad48879@gmail.com>
2024-01-13 00:15:05 -0500

    first attempt at displaying year.... more work to do with this

--- src/formatter.rs
+++ src/formatter.rs
@@ -22,7 +22,7 @@ pub fn format_months(config: AppConfig) -> Vec<String> {
 
 fn format_chunk(slice: &[MonthConfig]) -> Vec<String> {
     if slice.len() == 1 {
-        return format_month(&slice[0]);
+        return format_month(&slice[0], true);
     } else if slice.len() > 3 {
         panic!("don't know what to do with more than 3 slice len");
     } else if slice.is_empty() {
@@ -31,9 +31,12 @@ fn format_chunk(slice: &[MonthConfig]) -> Vec<String> {
         /* prepare output */
         let mut results = vec![];
 
+        /* determine if the year should go on the same line as the month */
+        let include_year_with_month_name = is_same_year(slice);
+
         /* get initial variables; for this case, there is at least 2 months */
-        let mut first = format_month(&slice[0]);
-        let mut second = format_month(&slice[1]);
+        let mut first = format_month(&slice[0], include_year_with_month_name);
+        let mut second = format_month(&slice[1], include_year_with_month_name);
         let mut largest = vec!(first.len(), second.len()).iter().max().unwrap().to_owned();
 
         /* the 3rd of 3 months is either a month, or just blank spaces */
@@ -42,7 +45,7 @@ fn format_chunk(slice: &[MonthConfig]) -> Vec<String> {
             extend(&mut output, largest);
             output
         } else {
-            let output = format_month(&slice[2]);
+            let output = format_month(&slice[2], include_year_with_month_name);
             largest = vec!(first.len(), second.len(), output.len()).iter().max().unwrap().to_owned();
             output
         };
@@ -64,12 +67,12 @@ fn format_chunk(slice: &[MonthConfig]) -> Vec<String> {
     }
 }
 
-fn format_month(config: &MonthConfig) -> Vec<String> {
+fn format_month(config: &MonthConfig, include_year: bool) -> Vec<String> {
     /* create initial variables */
     let first_day: i32 = (NaiveDate::from_ymd_opt(config.year as i32, config.month as u32, 1).unwrap().weekday().num_days_from_sunday() + 1) as i32;
     let mut next_index: i32 = 2 - first_day;
     let max: i32 = calc_days_in_month(config.month as u32, config.year as i32) as i32;
-    let month_name = month_name(&config, false);
+    let month_name = month_name(&config, include_year);
 
     /* create output lines var, and add static lines to it */
     let mut lines = vec![];
@@ -111,6 +114,13 @@ fn extend(vector: &mut Vec<String>, target_len: usize) {
     }
 }
 
+fn is_same_year(slice: &[MonthConfig]) -> bool {
+    return slice.iter()
+        .map(|f| f.year)
+        .collect::<std::collections::HashSet::<i16>>()
+        .len() == 1
+}
+
 #[cfg(test)]
 mod test {
     use crate::cfg_app::AppConfig;
@@ -125,7 +135,7 @@ mod test {
 
         assert_eq!(8, lines.len());
         assert_eq!("                     ", lines.get(0).unwrap());
-        assert_eq!("       January       ", lines.get(1).unwrap());
+        assert_eq!("    January 2023     ", lines.get(1).unwrap());
         assert_eq!(" Su Mo Tu We Th Fr Sa", lines.get(2).unwrap());
         assert_eq!("  1  2  3  4  5  6  7", lines.get(3).unwrap());
         assert_eq!("  8  9 10 11 12 13 14", lines.get(4).unwrap());
@@ -179,7 +189,7 @@ mod test {
         assert_eq!(" 26 27 28 29 30        24 25 26 27 28 29 30  28 29 30 31         ", lines.get(7).unwrap());
         assert_eq!("                       31                                        ", lines.get(8).unwrap());
         assert_eq!("                                                                 ", lines.get(9).unwrap());
-        assert_eq!("      February                March                              ", lines.get(10).unwrap());
+        assert_eq!("    February 2024          March 2024                            ", lines.get(10).unwrap());
         assert_eq!(" Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa                      ", lines.get(11).unwrap());
         assert_eq!("              1  2  3                  1  2                      ", lines.get(12).unwrap());
         assert_eq!("  4  5  6  7  8  9 10   3  4  5  6  7  8  9                      ", lines.get(13).unwrap());
