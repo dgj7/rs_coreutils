42b5b972d565a212a6ef65b66a94c584d8d0654f
dg <garciad48879@gmail.com>
2024-01-15 09:17:39 -0500

    skip the first line of output, which is a blank line; not present in normal     January 2024 Su Mo Tu We Th Fr Sa     1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31                        output

--- src/cal.rs
+++ src/cal.rs
@@ -2,5 +2,9 @@ use crate::{cfg_app, formatter};
 
 pub fn generate_cal(args: std::env::Args) -> Vec<String> {
     let config = cfg_app::AppConfig::new(args);
-    return formatter::format_from_app_config(config);
+    return formatter::format_from_app_config(config)
+        .iter()
+        .skip(1)
+        .map(|s| s.to_owned())
+        .collect();
 }
