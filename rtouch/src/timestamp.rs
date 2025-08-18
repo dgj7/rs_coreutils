85f2d63cda94a302b7080a6fc0c832c2346ea5db
dgj7 <garciad48879@gmail.com>
2025-08-17 20:45:19 -0400

    rtouch: documen timestamp.rs; add descritions for remaining unimplemented timestamp types

--- rtouch/src/timestamp.rs
+++ rtouch/src/timestamp.rs
@@ -2,12 +2,17 @@ use std::fs::File;
 use crate::config::Config;
 use std::time::SystemTime;
 
-
+///
+/// Storage for the timestamp; includes accessed and modified.
+///
 pub(crate) struct Timestamp {
     pub(crate) accessed: SystemTime,
     pub(crate) modified: SystemTime,
 }
 
+///
+/// Select the correct [Timestamp] based on configuration.
+///
 pub(crate) fn timestamp(config: &Config) -> Timestamp {
     if config.use_specified_time_stamp {
         specified_time_stamp(config)
@@ -23,16 +28,24 @@ pub(crate) fn timestamp(config: &Config) -> Timestamp {
     }
 }
 
+///
+/// Get the timestamp in the form \[\[CC]YY]MMDDhhmm\[.ss].
+///
 fn specified_time_stamp(_config: &Config) -> Timestamp {
     //config.specified_time_stamp.unwrap();
     panic!("not implemented"); // todo
 }
 
+///
+/// Get the timestamp from a date string, having various (human-readable) input formats.
+///
 fn date_string(_config: &Config) -> Timestamp {
     panic!("not implemented"); // todo
 }
 
-
+///
+/// Get the timestamp from a referenced file path.
+///
 fn reference_file(config: &Config) -> Timestamp {
     let path = config.reference_file.as_ref().unwrap();
     match File::open(path) {
